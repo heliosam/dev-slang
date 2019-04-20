89a9355d63c5a9aee707b1332dd62b6cc8629343
Helios <heliosaliaga@gmail.com>
2019-04-20 17:41:00 +0200

    Print English table

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import en from "./locales/en.json";
 
 class App extends Component {
   render() {
-    return <h1>Dev slang</h1>;
+    return (
+      <>
+        <h1>Dev slang</h1>
+        <table>
+          <tbody>
+            {Object.keys(en).map(key => (
+              <tr>
+                <td>{key}</td>
+                <td>{en[key].join(", ")}</td>
+              </tr>
+            ))}
+            <tr />
+          </tbody>
+        </table>
+      </>
+    );
   }
 }
 
