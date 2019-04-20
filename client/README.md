6bb48ee507f3ccff2d32d85549d5372a0a7781f2
Helios <heliosaliaga@gmail.com>
2019-04-20 17:27:28 +0200

    Create react app bootstraping with some cleanup

---
+++ client/README.md
@@ -0,0 +1,2 @@
+# Dev slang web
+The web client for dev slang. Bootstraped with create-react-app
---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <h1>Dev slang</h1>;
+  }
+}
+
+export default App;
