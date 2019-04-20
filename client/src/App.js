f516f22a311891c76cc7a2fc391a66598fa7c8f5
Helios <heliosaliaga@gmail.com>
2019-04-20 17:58:58 +0200

    Add language selector and update state accordingly

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import React, { Component } from "react";
-import en from "./locales/en.json";
+import React, { useState } from "react";
+import locales from "./locales";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <h1>Dev slang</h1>
-        <table>
-          <tbody>
-            {Object.keys(en).map(key => (
-              <tr>
-                <td>{key}</td>
-                <td>{en[key].join(", ")}</td>
-              </tr>
-            ))}
-            <tr />
-          </tbody>
-        </table>
-      </>
-    );
-  }
-}
+const App = () => {
+  console.log(locales);
+  const [locale, setLocale] = useState("en");
+  return (
+    <>
+      <h1>Dev slang</h1>
+      <label>Choose your language</label>
+      <select onChange={e => setLocale(e.target.value)}>
+        {Object.keys(locales).map(language => (
+          <option>{language}</option>
+        ))}
+      </select>
+      <table>
+        <tbody>
+          {Object.keys(locales[locale]).map(key => (
+            <tr>
+              <td>{key}</td>
+              <td>{locales[locale][key].join(", ")}</td>
+            </tr>
+          ))}
+          <tr />
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default App;
