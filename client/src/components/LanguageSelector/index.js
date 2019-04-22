7e96944420b805439e150083fd99665415faa232
Helios <heliosaliaga@gmail.com>
2019-04-22 17:16:01 +0200

    Add links, contributors and update Readme

--- client/src/components/LanguageSelector/index.js
+++ client/src/components/LanguageSelector/index.js
@@ -3,14 +3,19 @@ import "./styles.css";
 
 const LanguageSelector = ({ languages, setLanguage }) => (
   <div className="language-selector">
-    <label>Language</label>
-    <select onChange={e => setLanguage(e.target.value)}>
-      {languages.map(language => (
-        <option value={language.value} key={language.value}>
-          {language.name} {language.flag}
-        </option>
-      ))}
-    </select>
+    <div className="select-control">
+      <label>Language</label>
+      <select onChange={e => setLanguage(e.target.value)}>
+        {languages.map(language => (
+          <option value={language.value} key={language.value}>
+            {language.name} {language.flag}
+          </option>
+        ))}
+      </select>
+    </div>
+    <a className="link" href="https://github.com/heliosam/dev-slang#contribute">
+      Add new language
+    </a>
   </div>
 );
 
