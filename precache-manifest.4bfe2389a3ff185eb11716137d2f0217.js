9ffdc929adc5401d24132e859268a766bac9c435
Helios <heliosaliaga@gmail.com>
2019-04-23 22:15:21 +0200

    Updates

---
+++ precache-manifest.4bfe2389a3ff185eb11716137d2f0217.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a98ca3ca2d7cbe0d5b17",
+    "url": "/dev-slang/static/css/main.bcd723c9.chunk.css"
+  },
+  {
+    "revision": "a98ca3ca2d7cbe0d5b17",
+    "url": "/dev-slang/static/js/main.edb087ba.chunk.js"
+  },
+  {
+    "revision": "ae4cfdaa6127e8b5a5d3",
+    "url": "/dev-slang/static/js/runtime~main.7dcde4a7.js"
+  },
+  {
+    "revision": "2af48c7e119f2da43af5",
+    "url": "/dev-slang/static/js/2.fae1b156.chunk.js"
+  },
+  {
+    "revision": "074c75cec870047f7679c73aaf8abefe",
+    "url": "/dev-slang/index.html"
+  }
+];
